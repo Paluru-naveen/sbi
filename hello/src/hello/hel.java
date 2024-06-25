e965aafd3a7e36059ac406980a1bb4488c673bbf
paluru-naveen <naveenpaluru2000@gmail.com>
2024-06-25 19:08:21 +0530

    modified

--- hello/src/hello/hel.java
+++ hello/src/hello/hel.java
@@ -4,7 +4,7 @@ public class hel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello naveen");
+		System.out.println("hello naveen welcome");
 		
 
 	}
