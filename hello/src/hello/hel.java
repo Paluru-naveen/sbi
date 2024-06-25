0659433b9f2d7d2510533f5a52a9b2292482c9e2
Paluru-naveen <109218442+Paluru-naveen@users.noreply.github.com>
2024-06-25 20:08:02 +0530

    Update hel.java

--- hello/src/hello/hel.java
+++ hello/src/hello/hel.java
@@ -5,6 +5,8 @@ public class hel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello naveen welcome");
+		System.out.println("hello naveen welcome");
+		
 		
 
 	}
